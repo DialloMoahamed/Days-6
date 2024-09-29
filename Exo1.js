079e33d5833e382f3009930734aa5a3411efc990
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-09-29 16:48:29 +0100

    Question 1 terminer:Iterate 0 to 10 using for loop, do the same using while and do while loop

---
+++ Exo1.js
@@ -0,0 +1,20 @@
+// Reponse numero 1
+// --------- Itérer de 0 à 10 ---------// 
+// --------- Boucle for ---------// 
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+
+// --------- Boucle while ---------// 
+let i = 0;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+// --------- Boucle do while ---------// 
+let x = 0;
+do {
+    console.log(x);
+    x++
+} while (x <= 10)
