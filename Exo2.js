b9f13e6a9d9f922009fdd694b16e9b83c44be4d8
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-09-30 22:59:54 +0100

    Question 3 terminer:Écrivez un script qui génère un numéro de couleur RVB aléatoire.
    rgb(240,180,80)

--- Exo2.js
+++ Exo2.js
@@ -10,11 +10,20 @@ console.log(randomId);
 
 
 // Reponse numero 2
-const characterse = '#ee33df';
+const characterse = '#ee33df'
 let randomIde = '';
 for (let r = 0; r < 7; r++) {
-    let randomIndexe = Math.floor(Math.random() * characterse.length);
-    randomIde += characterse.charAt(randomIndexe);
+    let randomInde = Math.floor(Math.random() * characterse.length);
+    randomIde += characterse.charAt(randomInde);
     
 };
 console.log(randomIde);
+
+
+// Reponse numero 3
+let r = Math.floor(Math.random() * 256);
+let g = Math.floor(Math.random() * 256);
+let b = Math.floor(Math.random() * 256);
+
+let couleur = "rgb(" + r + "," + g + "," + b + ")";
+console.log(couleur);
