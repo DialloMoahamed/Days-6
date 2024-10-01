44bf2a6aefce3e7093f58f60c57c06baf777ab79
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-10-01 19:48:57 +0100

    Question 2 terminer:Arrays are mutable. Create a copy of array which does not modify the original. Sort the copied array and store in a variable sortedCountries

--- Exo3.js
+++ Exo3.js
@@ -17,3 +17,15 @@ for (let i = 0; i < countries.length; i++) {
     table.push(countries[i]) ;
 }
 console.log(table);
+
+
+
+// Reponse numero 2
+let tableA = [];
+for (let j = 0; j < countries.length; j++) {
+    tableA.push(countries[j]) ;
+}
+console.log(table);
+
+let sortedCountries = tableA.sort();
+console.log(sortedCountries);
