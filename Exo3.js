ed820b2c2bd8a16f927514df27ad5df79e08b6db
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-10-01 19:40:57 +0100

    Question 1 terminer

---
+++ Exo3.js
@@ -0,0 +1,19 @@
+// Reponse numero 1
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+let table = [];
+for (let i = 0; i < countries.length; i++) {
+    table.push(countries[i]) ;
+}
+console.log(table);
